6321e9a621dc573ee5c33e318b30f4fb161b729f
plusn <91170882+plusn5@users.noreply.github.com>
2022-05-19 17:27:29 +0800

    add week13

---
+++ week13/week13.js
@@ -0,0 +1,36 @@
+
+function setup() {
+    initializeFields();
+    // 只做一次
+    createCanvas(300, 300);
+    fill(color(0x97, 0xC4, 0xFA));
+    textSize(50);
+    textAlign(CENTER, CENTER);
+}
+
+// -1=沒選
+var choice;
+
+function draw() {
+    // 每秒60次
+    background(color(0xFF, 0xFA, 0xE8));
+    text("Dinner 1", 150, 50);
+    text("Dinner 2", 150, 150);
+    text("Dinner 3", 150, 250);
+    // ellipse圓
+    if (choice == 0)
+        ellipse(30, 50, 50, 50);
+    if (choice == 1)
+        ellipse(30, 150, 50, 50);
+    if (choice == 2)
+        ellipse(30, 250, 50, 50);
+}
+
+function mousePressed() {
+    // 取出整數0,1,2
+    choice = int(random(3));
+}
+
+function initializeFields() {
+    choice = -1;
+}
